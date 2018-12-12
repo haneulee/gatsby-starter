7aa635053bad3c88b15669e7391b92f08512c6b5
haneulee <lovesky4294@gmail.com>
2018-12-12 18:56:00 +0900

    removed manifest icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,8 @@ module.exports = {
         theme_color: "#6b37bf",
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "src/images/icon.png" // This path is relative to the root of the site.
+        display: "standalone"
+        // icon: "src/images/icon.png" // This path is relative to the root of the site.
       }
     },
     `gatsby-plugin-offline`,
