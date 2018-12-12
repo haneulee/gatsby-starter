f61ed518b476a2235039e1a23c4d2d173ea799f8
haneulee <lovesky4294@gmail.com>
2018-12-12 17:53:50 +0900

    created pages from datas

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,5 @@
 import Typography from "typography";
-import bootstrapTheme from "typography-theme-bootstrap";
+// import bootstrapTheme from "typography-theme-bootstrap";
 // import lawtonTheme from "typography-theme-lawton"
 // import fairyGateTheme from "typography-theme-fairy-gates";
 import kirkhamTheme from "typography-theme-kirkham";
