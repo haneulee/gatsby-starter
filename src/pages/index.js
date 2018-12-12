a6552a9a297dfa58043c8b7771d99264cb9d1d78
haneulee <lovesky4294@gmail.com>
2018-12-12 15:03:24 +0900

    made nested layout components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Layout from "../components/layout";
 
 export default () => (
-  <div style={{ color: `purple` }}>
-    <h1>Hello Gatsby!</h1>
-    <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />{" "}
-  </div>
+  <Layout>
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />{" "}
+    </div>
+  </Layout>
 );
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,9 @@
 import Typography from "typography";
 import bootstrapTheme from "typography-theme-bootstrap";
 // import lawtonTheme from "typography-theme-lawton"
+import fairyGateTheme from "typography-theme-fairy-gates";
 
-const typography = new Typography(bootstrapTheme);
+const typography = new Typography(fairyGateTheme);
 
 // const typography = new Typography({ baseFontSize: "18px" });
 
